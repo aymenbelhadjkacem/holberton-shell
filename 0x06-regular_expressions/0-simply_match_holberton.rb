0b6397aa15c4c2272bf3538577ae58aab5283d39
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-07-16 13:10:43 -0700

    zz

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#CReating a regular expresion to find A SIMPLY matching holberton
+# CReating a regular expresion to find A SIMPLY matching holberton
 puts ARGV[0].scan(/Holberton/).join
