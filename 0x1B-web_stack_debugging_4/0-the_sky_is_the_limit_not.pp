bb957529b64f1d8bf175b15edbb5c4760a3c3f33
aymenbelhadjkacem <aymenbelhadjkacem>
2021-11-05 07:45:49 -0400

    fixed*

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# fix problem with multiple open files in nginx
+#fixproblem with multiple open files in nginx
 exec{ 'fix--for-nginx':
     command => 'echo "ULIMIT=\"-n 2000\"" > /etc/default/nginx && /etc/init.d/nginx restart',
     path    => '/usr/local/bin/:/bin/',
